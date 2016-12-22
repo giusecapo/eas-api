a4e3559734663fade45c9ca9d76db1635d29d592
Giuseppe Capoluongo <giuse_capo@icloud.com>
2016-12-22 21:10:02 +0100

    cloudddd

--- cloud/main.js
+++ cloud/main.js
@@ -147,7 +147,7 @@ Parse.Cloud.define("getRequestList", function(req, res){
 	  	console.log("getRequestList > FOUND " + results.length + " OBJECTS")
 	  	console.log(JSON.stringify(results))
 	  	let requests = results
-	  	for(let request of results){
+	  	for(let request of requests){
 	  		var title = request.get("subject")
 	  		var from = request.get("from")
 	  		var fromCountry = from.get("country")
