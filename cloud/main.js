e9f8a748291bce297d0296b612a023df3991ae8a
Giuseppe Capoluongo <giuse_capo@icloud.com>
2016-12-22 21:01:16 +0100

    fixes

--- cloud/main.js
+++ cloud/main.js
@@ -146,7 +146,7 @@ Parse.Cloud.define("getRequestList", function(req, res){
 	  success: function(results) {
 	  	console.log("getRequestList > FOUND " + results.length + " OBJECTS")
 	  	console.log(JSON.stringify(results))
-	  	for(request in result){
+	  	for(request in results){
 	  		console.log(i)
 	  // 		var title = request.get("subject")
 	  // 		var from = request.get("from")
